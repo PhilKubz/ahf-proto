856ddb77161d6af8570cd57f5ca56b1b75e246ad
PhilKubsz <philip.kubisz@gmail.com>
2023-11-01 23:06:58 -0700

    initial commit / file structure

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ahf-proto
+
+Prototype Website for a client
+
+Wireframe / file structure first, then create components and styling for frontend
---
+++ ahf-proto/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>AHF Prototype</h1>
+
+      {/* Header */}
+      {/* Content */}
+      {/* Footer */}
+
+      {/* About */}
+      {/* Store Locator */}
+    </div>
+  );
+}
+
+export default App;
